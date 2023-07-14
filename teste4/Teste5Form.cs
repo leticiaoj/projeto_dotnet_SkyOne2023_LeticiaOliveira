f8040da5f0af55cd94b0eee212e03e7af3c40144
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-07-14 20:20:51 -0300

    Adição dos placeholders nos campos do Teste 5.

--- teste4/Teste5Form.cs
+++ teste4/Teste5Form.cs
@@ -39,45 +39,48 @@ namespace teste4
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new Size(200, 20);
             this.txtNome.TabIndex = 0;
+            this.txtNome.PlaceholderText = "Nome do Aluno";
 
             // Configuração do TextBox de Nota1
             this.txtNota1.Location = new Point(100, 80);
             this.txtNota1.Name = "txtNota1";
             this.txtNota1.Size = new Size(200, 20);
             this.txtNota1.TabIndex = 1;
+            this.txtNota1.PlaceholderText = "Nota do 1º Bimestre:";
 
             // Configuração do TextBox de Nota2
             this.txtNota2.Location = new Point(100, 110);
             this.txtNota2.Name = "txtNota2";
             this.txtNota2.Size = new Size(200, 20);
             this.txtNota2.TabIndex = 2;
+            this.txtNota2.PlaceholderText = "Nota do 2º Bimestre:";
 
             // Configuração da Label de erro do Nome
             this.lblNomeErro.AutoSize = true;
             this.lblNomeErro.ForeColor = Color.Red;
             this.lblNomeErro.Location = new Point(310, 53);
             this.lblNomeErro.Name = "lblNomeErro";
-            this.lblNomeErro.Size = new Size(80, 13);
+            this.lblNomeErro.Size = new Size(100, 13);
             this.lblNomeErro.TabIndex = 3;
-            this.lblNomeErro.Text = "Campo obrigatório";
+            this.lblNomeErro.Text = "O nome é obrigatório.";
 
             // Configuração da Label de erro da Nota1
             this.lblNota1Erro.AutoSize = true;
             this.lblNota1Erro.ForeColor = Color.Red;
             this.lblNota1Erro.Location = new Point(310, 83);
             this.lblNota1Erro.Name = "lblNota1Erro";
-            this.lblNota1Erro.Size = new Size(80, 13);
+            this.lblNota1Erro.Size = new Size(100, 13);
             this.lblNota1Erro.TabIndex = 4;
-            this.lblNota1Erro.Text = "Campo obrigatório";
+            this.lblNota1Erro.Text = "A nota é obrigatória";
 
             // Configuração da Label de erro da Nota2
             this.lblNota2Erro.AutoSize = true;
             this.lblNota2Erro.ForeColor = Color.Red;
             this.lblNota2Erro.Location = new Point(310, 113);
             this.lblNota2Erro.Name = "lblNota2Erro";
-            this.lblNota2Erro.Size = new Size(80, 13);
+            this.lblNota2Erro.Size = new Size(100, 13);
             this.lblNota2Erro.TabIndex = 5;
-            this.lblNota2Erro.Text = "Campo obrigatório";
+            this.lblNota2Erro.Text = "A nota é obrigatória.";
 
             // Configuração do botão Limpar
             this.btnLimpar.Location = new Point(100, 150);
@@ -114,16 +117,19 @@ namespace teste4
             this.Controls.Add(this.txtNota2);
             this.Controls.Add(this.txtNota1);
             this.Controls.Add(this.txtNome);
-            this.Name = "Teste05Form";
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Lançamento de Notas";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
         {
-            LimparCampos();
+            txtNome.Text = "";
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            lblResultado.Text = "";
+            lblNomeErro.Visible = false;
+            lblNota1Erro.Visible = false;
+            lblNota2Erro.Visible = false;
         }
 
         private void btnEnviar_Click(object sender, EventArgs e)
@@ -167,33 +173,25 @@ namespace teste4
             if (camposPreenchidos)
             {
                 double media = (Convert.ToDouble(nota1) + Convert.ToDouble(nota2)) / 2;
+                string status;
 
                 if (media < 6.5)
                 {
                     lblResultado.ForeColor = Color.Red;
-                    lblResultado.Text = "Reprovado";
+                    status = "Reprovado";
                 }
                 else
                 {
                     lblResultado.ForeColor = Color.Green;
-                    lblResultado.Text = "Aprovado";
+                    status = "Aprovado";
                 }
+
+                lblResultado.Text = "Status do Aluno: " + status;
             }
             else
             {
-                lblResultado.Text = "";
+                lblResultado.Text = "Nota não enviada.";
             }
-        }
-
-        private void LimparCampos()
-        {
-            txtNome.Text = "";
-            txtNota1.Text = "";
-            txtNota2.Text = "";
-            lblNomeErro.Visible = false;
-            lblNota1Erro.Visible = false;
-            lblNota2Erro.Visible = false;
-            lblResultado.Text = "";
         }
     }
 }

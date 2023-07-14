0b6958b87940635fc0c9dc13bd56c1cf4da80cc0
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-07-14 18:21:35 -0300

    Teste 1 - Média do Aluno

---
+++ teste1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite a nota do primeiro semestre:");
+decimal notaSemestre1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a nota do segundo semestre:");
+decimal notaSemestre2 = Convert.ToDecimal(Console.ReadLine());
+
+decimal mediaAluno = (notaSemestre1 + notaSemestre2) / 2;
+
+Console.WriteLine("Sua média é de " + mediaAluno.ToString("0.00"));
---
+++ teste4/Form1.cs
@@ -0,0 +1,9 @@
+namespace teste4;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
